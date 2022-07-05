6b215452c4d499ceb238a9d6f19d888a818f1b9c
rejan bajracharya <rejandev@gmail.com>
2022-07-05 20:27:20 +0545

    make login page

---
+++ src/components/CheckboxLabel.jsx
@@ -0,0 +1,9 @@
+// import * as React from 'react';
+// import { FormGroup, FormControlLabel, Checkbox } from '@mui/material';
+// export default function CheckboxLabels({label}) {
+//   return (
+//     <FormGroup>
+//       <FormControlLabel control={<Checkbox defaultChecked />} label={label} />
+//     </FormGroup>
+//   );
+// }
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Box, Container, Typography, Button, TextField, Link } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+
+
+/* import custom components */
+// import CheckboxLabel from '../../components/CheckboxLabel';
+import {Wrapper, Icon, CustomBox, CustomButton, CustomLink, CustomTextField, styles, color, HeadingMain, HeadingSecondary} from './Login.styles'
+
+
+const Login = () => {
+  return (
+    <>
+      <Container maxWidth='sm'>
+        <Wrapper>
+          <>
+            <Typography variant='h6' gutterBottom={true} align='center' sx={styles.branding}>
+              <Icon>
+                <i className='fa-brands fa-raspberry-pi'></i>
+              </Icon>
+              EB THEME
+            </Typography>
+          </>
+
+          <CustomBox>
+            <HeadingMain variant='h6'>Hi, Welcome Back</HeadingMain>
+            <HeadingSecondary variant='p'>Enter your credentials to continue</HeadingSecondary>
+          </CustomBox>
+
+          <CustomButton variant='outlined' fullWidth>
+            <img
+              src='/assets/google.svg'
+              alt='google'
+              width='16'
+              height='16'
+              style={{ marginRight: '8px' }}
+            />
+            Sign In With Google
+          </CustomButton>
+
+          <CustomBox>
+            <Button variant='outlined' disabled sx={styles.ORButton}>
+              OR
+            </Button>
+          </CustomBox>
+
+          <CustomBox>
+            <Typography varaint='h6' sx={{color: 'rgb(0,0,0)', fontSize: '0.875rem'}}>Sign in with Email address</Typography>
+          </CustomBox>
+
+          <CustomTextField variant='outlined' required label='Email Address / Username' fullWidth type='text' ></CustomTextField>
+          <CustomTextField variant='outlined' required label='Password' fullWidth type='password' sx={{marginBottom: '1rem'}}></CustomTextField>
+
+          <CustomBox>
+            {/* <CheckboxLabel label='Remember me' /> */}
+            <CustomLink component={RouterLink} to='/forgotPassword'>
+              Forgot Password?
+            </CustomLink>
+          </CustomBox>
+
+          <Button variant='contained' fullWidth={true} sx={styles.SubmitButton} >
+            Sign In
+          </Button>
+
+          <hr />
+
+          <CustomLink component={RouterLink} to='/register'>
+            Don't have an account?
+          </CustomLink>
+        </Wrapper>
+      </Container>
+    </>
+  );
+};
+
+
+
+
+
+export default Login;
