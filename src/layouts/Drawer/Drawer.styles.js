895dfc6ea96332eb91d3630b4125133c679adc8a
rejan bajracharya <rejandev@gmail.com>
2022-07-08 16:51:04 +0545

    add basic stylings on drawer

--- src/layouts/Drawer/Drawer.styles.js
+++ src/layouts/Drawer/Drawer.styles.js
@@ -1,29 +1,55 @@
-import {Drawer, List, ListItem, ListItemIcon, ListItemText, ListItemButton, styled} from '@mui/material'; 
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListItemButton,
+  styled,
+  Typography,
+  Icon,
+} from '@mui/material';
 
-export const SideMenu = styled(({...otherProps}) => <Drawer {...otherProps} />)`
-position: relative; 
-top: 20; 
-`
+export const SideMenu = styled(({ ...otherProps }) => <Drawer {...otherProps} />)`
+  position: relative;
+  top: 20;
+`;
 
-export const AppList = styled(({...otherProps}) => <List {...otherProps} />)`
-background-color: #f3f3f3;
+export const AppList = styled(({ ...otherProps }) => <List {...otherProps} />)`
+  // background-color: #f3f3f3;
+`;
 
-`
+export const AppListItem = styled(({ ...otherProps }) => <ListItem {...otherProps} />)`
+  // background-color: #f3f3f3;
+`;
 
-export const AppListItem = styled(({...otherProps}) => <ListItem {...otherProps} />)`
-background-color: #f3f3f3;
+export const AppListButton = styled(({ ...otherProps }) => <ListItemButton {...otherProps} />)`
+  padding: 8px 1rem;
+  background: #fefefe;
+  color: #777779;
+  border-radius: 10px;
+  :hover {
+    background-color: #e0f2f2;
+    color: #26a59a;
+  }
+`;
 
-`
+export const AppListIcon = styled(({ ...otherProps }) => <ListItemIcon {...otherProps} />)``;
 
-export const AppListButton = styled(({...otherProps}) => <ListItemButton {...otherProps} />)`
-background-color: #f3f3f4;
+export const AppListText = styled(({ ...otherProps }) => <ListItemText {...otherProps} />)`
+   ;
+`;
 
-`
+export const BrandText = styled(Typography)`
+  fontweight: 600;
+  color: #000;
+`;
 
-export const AppListIcon = styled(({...otherProps}) => <ListItemIcon {...otherProps} />)`
-color: red; 
-`
+export const BrandIcon = styled(({ ...otherProps }) => <Icon {...otherProps} />)`
+height: 2.5rem; 
+transform: translateY(40%); 
+margin-right: 0.3rem;
+color: #26a59a; 
 
-export const AppListText = styled(({...otherProps}) => <ListItemText {...otherProps} />)`
-color: red;
-`
+`;
+
--- src/layouts/Drawer/index.jsx
+++ src/layouts/Drawer/index.jsx
@@ -1,41 +1,51 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail'
+import MailIcon from '@mui/icons-material/Mail';
 import { AppBarContext } from '../../context/AppBarContext';
-import { AppList, AppListButton, AppListIcon, AppListItem, AppListText, SideMenu } from './Drawer.styles';
-
-
-
+import {
+  AppList,
+  AppListButton,
+  AppListIcon,
+  AppListItem,
+  AppListText,
+  BrandText,
+  SideMenu,
+  BrandIcon
+} from './Drawer.styles';
+import { Icon } from '@mui/material';
 
 const ItemsList = [
-  {icon: <InboxIcon />, text: 'Page Management', path: '/dashboard/pageManagement'}, 
-  {icon: <MailIcon/>, text: 'Admin Management', path: '/dashboard/adminManagement'}, 
-]
+  { icon: <InboxIcon />, text: 'Page Management', path: '/dashboard/pageManagement' },
+  { icon: <MailIcon />, text: 'Admin Management', path: '/dashboard/adminManagement' },
+];
 
 const AppDrawer = () => {
-  const {sidebarOpen} = useContext(AppBarContext); 
-  const navigate = useNavigate(); 
+  const { sidebarOpen } = useContext(AppBarContext);
+  const navigate = useNavigate();
   return (
-    <div sx={{position:'relative'}}>
-    <SideMenu variant='persistent' anchor='right' open={sidebarOpen}>
-       <AppList>
-         {ItemsList.map(({icon, text, path}) => (
+    <div sx={{ position: 'relative' }}>
+      <SideMenu variant='persistent' anchor='right' open={sidebarOpen}>
+        <BrandText variant='body1' component='p' gutterBottom={true} align='center'>
+          <BrandIcon >
+            <i className='fa-brands fa-raspberry-pi'></i>
+          </BrandIcon>
+          EB THEME
+        </BrandText>
+        <AppList>
+          {ItemsList.map(({ icon, text, path }) => (
             <AppListItem key={text}>
               <AppListButton onClick={() => navigate(path)}>
-                <AppListIcon>
-                  {icon}
-                </AppListIcon>
+                <AppListIcon>{icon}</AppListIcon>
                 <AppListText primary={text} />
               </AppListButton>
             </AppListItem>
           ))}
         </AppList>
-    </SideMenu>
+      </SideMenu>
     </div>
-    
-  )
-}
+  );
+};
 
-export default AppDrawer
+export default AppDrawer;
