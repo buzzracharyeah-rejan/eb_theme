ebdc05388d3a1adc07fd26b8cca72955212e4eba
rejan bajracharya <rejandev@gmail.com>
2022-07-11 22:01:08 +0545

    made style issue fix on header

--- src/layouts/SidebarLayout/Header/Header.styles.js
+++ src/layouts/SidebarLayout/Header/Header.styles.js
@@ -1,11 +1,4 @@
-import {
-  Badge,
-  IconButton,
-  AppBar as MuiAppBar,
-  Typography,
-  Toolbar,
-  Drawer as MuiDrawer,
-} from '@mui/material';
+import { AppBar as MuiAppBar, Toolbar as MuiToolbar } from '@mui/material';
 import { styled } from '@mui/material';
 
 import { drawerWidth } from '../consts';
@@ -14,22 +7,40 @@ const closedMixin = (theme) => ({
   transition: theme.transitions.create(['width', 'margin'], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
-})
-})
+  }),
+});
 
-const openMixin = theme => ({
+const openMixin = (theme) => ({
   transition: theme.transitions.create(['width', 'margin'], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
   }),
-})
+});
 
 export const AppBar = styled(MuiAppBar)(({ theme, ...otherProps }) => ({
   zIndex: theme.zIndex.drawer + 1,
-  ...closedMixin(theme), 
+  ...closedMixin(theme),
   ...(otherProps.open && {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
-    ...openMixin(theme)
+    ...openMixin(theme),
   }),
-}))
+}));
+
+export const Toolbar = styled(MuiToolbar)(({ theme, ...otherProps }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+}));
+
+export const MenuMapBrand = styled('div')(({ theme, ...otherProps }) => ({
+  display: 'flex',
+  background: '#f3f3f3',
+}));
+
+export const BrandWrap = styled('div')(({ theme }) => ({
+  display: 'flex',
+  gap: '1rem',
+  justifyContent: 'center',
+  alignItems: 'center',
+}));
--- src/layouts/SidebarLayout/Header/index.js
+++ src/layouts/SidebarLayout/Header/index.js
@@ -1,15 +1,14 @@
 import React, { useState, useContext } from 'react';
-import { Badge, IconButton, AppBar as MuiAppBar, Typography, Toolbar } from '@mui/material';
+import { Badge, IconButton, AppBar as MuiAppBar, Typography } from '@mui/material';
 
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 
-import {AppBar, Drawer} from './Header.styles'; 
+import { AppBar, BrandWrap, Drawer, MenuMapBrand, Toolbar } from './Header.styles';
 import { SidebarContext } from '../../../context/SidebarContext';
 
 const drawerWidth = 240;
-
 
 const Header = () => {
   const { open, toggleSidebar } = useContext(SidebarContext);
@@ -17,12 +16,16 @@ const Header = () => {
   return (
     <AppBar open={open} position='absolute' elevation={0}>
       <Toolbar>
-        <IconButton onClick={toggleSidebar}>
-          <MenuIcon />
-        </IconButton>
-        <Typography variant='h6' component='h1'>EBTHEME</Typography>
+        <BrandWrap>
+          <IconButton onClick={toggleSidebar}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' component='h1'>
+            EB THEME
+          </Typography>
+        </BrandWrap>
         <IconButton>
-          <NotificationsIcon /> 
+          <NotificationsIcon />
         </IconButton>
       </Toolbar>
     </AppBar>
